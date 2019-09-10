da392a2a0c279fa4c2a411fa261a3b3fdb795965
Lucasolifreitas <lucasolifreitas@outlook.com>
2019-09-10 13:40:01 -0300

    Exercícios com fininalidade de por em prática conhecimentos adquidiros com a linguagem Python.

--- secao4/exerc3.py
+++ secao4/exerc3.py
@@ -1,3 +1,5 @@
+"peça ao usuário para digitar tres valores inteiros e imprima a soma deles"
+
 num1 = int(input("Digite um numero inteiro: "))
 num2 = int(input("Digite um segundo numero inteiro: "))
 num3 = int(input("Digite um terceiro numero inteiro: "))
